90fc31b977e9ee16e3f9e33b74559dc2821d49a0
Guy <153140996+pretzer25@users.noreply.github.com>
2024-07-15 10:25:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const puppeteer = require('puppeteer');
 const WebSocket = require('ws');
-const fs = require('fs');
-const path = require('path');
-const http = require('http');
-const https = require('https');
 
 let browser, page;
 
@@ -17,7 +13,7 @@ async function launchBrowser() {
 }
 
 // WebSocket server setup
-const wss = new WebSocket.Server({ noServer: true });
+const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', (ws) => {
   ws.on('message', async (message) => {
@@ -32,37 +28,6 @@ wss.on('connection', (ws) => {
   });
 });
 
-// HTTP server to serve static files and handle WebSocket upgrade
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    const indexPath = path.join(__dirname, 'public', 'index.html');
-    fs.readFile(indexPath, (err, data) => {
-      if (err) {
-        res.writeHead(500);
-        res.end('Error loading index.html');
-        return;
-      }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(data);
-    });
-  } else {
-    res.writeHead(404);
-    res.end('Not Found');
-  }
-});
-
-server.on('upgrade', (request, socket, head) => {
-  wss.handleUpgrade(request, socket, head, (ws) => {
-    wss.emit('connection', ws, request);
-  });
-});
-
 launchBrowser().catch(console.error);
 
-server.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
-
-module.exports = (req, res) => {
-  res.status(200).send('Puppeteer WebSocket server is running');
-};
+console.log('WebSocket server is running on port 8080');
